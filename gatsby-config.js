3d32acf029385552c1dcf99c3ef292aba4802a2f
thomasms <stainer.tom@gmail.com>
2019-09-09 20:26:03 +0100

    Added Google analytics to blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-147422535-1`,
       },
     },
     `gatsby-plugin-feed`,
