4c5d19f5b5373aadffccc9e081717b5579601c66
thomasms <stainer.tom@gmail.com>
2019-09-11 22:52:22 +0100

    Spelling corrects - I really can't spelldjsadsak

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,13 +14,13 @@ class NotFoundPage extends React.Component {
         <SEO title="About" />
         <h4>Some details about me....</h4>
         <p>
-            I did studied physics and some mathematics a while ago. 
+            I studied physics and mathematics a while ago. 
         </p>
         <p>
             I liked it so much I did a PhD in Particle Physics, during which I did a lot of programming.
         </p>
         <p>
-            I then realised how powerful programming was and got a role as a Software Engineer at a company doing some stuff in Oil and Gas.
+            I then realised how powerful programming was and got a role as a Software Engineer at a company doing some stuff related to petrochemicals.
         </p>
         <p>
             I then learnt how to actually program, including all about testing and building production software.
@@ -29,7 +29,7 @@ class NotFoundPage extends React.Component {
             After some time I wanted a new challenge, and missed physics a bit.
         </p>
         <p>
-            Now I am a nuclear scientist / software engineer, where I get to do programing, physics, and research!
+            Now I am a nuclear scientist / software engineer, where I get to do: programming, physics, and research!
         </p>
       </Layout>
     )
