37e731297a5e7548834fb05286b74bff8b2497d6
thomasms <stainer.tom@gmail.com>
2022-01-07 11:41:36 +0100

    Update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,9 +19,8 @@ class NotFoundPage extends React.Component {
           a lot of programming.
         </p>
         <p>
-          I then realised how powerful programming was and got a role as a
-          Software Engineer at a company doing some stuff related to
-          petrochemicals.
+          Realising how powerful programming was I got a role as a Software
+          Engineer at a company doing some stuff related to petrochemicals.
         </p>
         <p>
           I then learnt how to <i>actually</i> program, including all about
@@ -33,11 +32,22 @@ class NotFoundPage extends React.Component {
         <p>
           Going back to physics I spent some time as a nuclear scientist /
           software engineer, where I got to do some work on some interesting
-          projects in fusion research.
+          projects in fusion research at UKAEA.
         </p>
         <p>
-          Now I am a data scientist building ML algorithms for HR (while trying
-          to do some nuclear research on the side).
+          I dabbled in HR as a data scientist building ML algorithms and doing
+          application development (while trying to do some nuclear research on
+          the side).
+        </p>
+        <p>
+          Subsequently I joined the MYRRHA project and lead the software
+          development of the control system at SCK CEN. After a while I got very
+          frustrated with the bureaucracy and resigned.
+        </p>
+        <p>
+          Now I am in a totally new domain of health care, again leading
+          software development for a great start-up, spending considerable time
+          writing Python and developing ETLs.
         </p>
       </Layout>
     )
